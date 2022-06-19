f72378f7631637c37faffabb1e78411f6c3a3641
tyakamyz <tyakamyz@naver.com>
2022-06-19 18:11:12 +0900

    feat: OrderRepository 구현

---
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+    private final EntityManager em;
+
+    public Long save(Order order) {
+        em.persist(order);
+        return order.getId();
+    }
+
+    public Order findOne(Long orderId) {
+        return em.find(Order.class, orderId);
+    }
+
+    // public List<Order> findAll(OrderSearch orderSearch) {}
+}
