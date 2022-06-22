937c292c61d2b1b66972891caa8046250976283b
tyakamyz <tyakamyz@naver.com>
2022-06-22 22:00:52 +0900

    feat: 상품 수정 화면 구현

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -1,11 +1,14 @@
 package jpabook.jpashop.controller;
 
 import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -38,5 +41,36 @@ public class ItemController {
         model.addAttribute("items", itemService.findItems());
 
         return "items/itemList";
+    }
+
+    @GetMapping("/items/{itemId}/edit")
+    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model) {
+        Book findItem = (Book) itemService.findOne(itemId);
+
+        BookForm form = new BookForm();
+        form.setId(findItem.getId());
+        form.setName(findItem.getName());
+        form.setPrice(findItem.getPrice());
+        form.setStockQuantity(findItem.getStockQuantity());
+        form.setAuthor(findItem.getAuthor());
+        form.setIsbn(findItem.getIsbn());
+
+        model.addAttribute("form", form);
+        return "items/updateItemForm";
+    }
+
+    @PostMapping("/items/{itemId}/edit")
+    public String updateItem(@ModelAttribute("form") BookForm bookForm) {
+        Book book = new Book();
+
+        book.setId(bookForm.getId());
+        book.setName(bookForm.getName());
+        book.setPrice(bookForm.getPrice());
+        book.setStockQuantity(bookForm.getStockQuantity());
+        book.setAuthor(bookForm.getAuthor());
+        book.setIsbn(bookForm.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/items";
     }
 }
